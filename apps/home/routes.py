dd3068a9779dc109764636fef07dad87546e9ed6
Ninz <ninn.tav@gmail.com>
2024-06-23 22:51:06 +0700

    update template

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -3,7 +3,7 @@
 from apps.home import blueprint
 from flask import render_template,request
 from flask_login import login_required
-@blueprint.route('/index')
+@blueprint.route('/dashboard')
 @login_required
 def index():
     return render_template('home/index.html')
